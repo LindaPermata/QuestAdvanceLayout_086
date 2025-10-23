2c614403c54c6d70b5b0f94185713842ced43e94
LindaPermata <permatalinda531@gmail.com>
2025-10-23 11:34:52 +0700

    Membuat code untuk memanggil id prodi

--- app/src/main/java/com/example/questadvancelayout_086/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_086/UIPertama.kt
@@ -2,11 +2,17 @@ package com.example.questadvancelayout_086
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
     Column(modifier = Modifier.padding(top=100.dp)
         .fillmax(),
-        horizontalAllignment = Alignment.CenterHorizontally)
+        horizontalAllignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = StringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontweight = FontWeight.Bold)
+    }
 }

e260fe7f6ad495c3120ae7f25a73a939c90ece3a
LindaPermata <permatalinda531@gmail.com>
2025-10-23 11:56:44 +0700

    Menambahkan Gambar di dalam Row

--- app/src/main/java/com/example/questadvancelayout_086/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_086/UIPertama.kt
@@ -2,15 +2,20 @@ package com.example.questadvancelayout_086
 
 import android.R.attr.height
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -28,13 +33,23 @@ fun ActivitasPertama(modifier: Modifier) {
             fontweight = FontWeight.Bold)
         Text(stringResource( R.string.univ),
             fontSize = 22.sp)
-        Spacer(modifier = Modifier, height(25.dp))
+        Spacer(modifier = Modifier.height(25.dp))
         Card(modifier = Modifier
             .fillMaxWidth(1f)
             .padding(12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
-            )
-        )
+            )) {
+            Row() {
+                val gambar = painterResource(R.drawable.logo)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
+
+            }
+
+        }
     }
 }
